556a22127b6c288bfb95c0641b12ab69ce579ca6
Tremaine Island <t.island84@gmail.com>
2020-06-14 00:51:34 -0600

    added bootstrap

--- flask_blog/blog.py
+++ flask_blog/blog.py
@@ -7,4 +7,17 @@ bp = Blueprint('blog', __name__)
 
 @bp.route('/')
 def index():
-    return render_template('home.html', title="Home Page")
+    summary = """
+    Sony has finally shown off what the PlayStation 5 will look like, but there’s one very big question about the design
+    that’s still unclear: just how big is the PlayStation 5?
+    We’ll likely have to wait until Sony gives official dimensions (or shows the physical box next to something else 
+    for scale) of the controversial design to know for sure, but that isn’t stopping fans on the internet from doing their 
+    best to estimate things. And based on early guesses, the PS5 is going to be huge — perhaps even the 
+    biggest mainstream console in years.
+    """
+
+    feature = {
+        "title": "The PS5 is huge, according to internet detectives",
+        "summary": f'{summary[:200]} ...'
+    }
+    return render_template('blog/index.html', title="My Blog Page", feature=feature)
