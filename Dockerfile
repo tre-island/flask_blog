e23df8395d3b0d547240276015a30523dddf6a36
Tremaine Island <t.island84@gmail.com>
2020-06-14 21:30:11 -0600

    updated Dockerfile to make it more generic for use with other projects

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM python:latest
 RUN apt update; mkdir -p /app
 COPY . /app
 WORKDIR "/app"
-EXPOSE 5000
 RUN pip install -r requirements.txt
 ENTRYPOINT ["python"]
 
--- flask_blog/blog.py
+++ flask_blog/blog.py
@@ -8,8 +8,6 @@ bp = Blueprint('blog', __name__)
 @bp.route('/')
 def index():
     summary = """
-    Sony has finally shown off what the PlayStation 5 will look like, but there’s one very big question about the design
-    that’s still unclear: just how big is the PlayStation 5?
     We’ll likely have to wait until Sony gives official dimensions (or shows the physical box next to something else 
     for scale) of the controversial design to know for sure, but that isn’t stopping fans on the internet from doing their 
     best to estimate things. And based on early guesses, the PS5 is going to be huge — perhaps even the 
